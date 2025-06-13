cb4d9750d5a5906164135ccb8864116b5e8381f5
oshinono <v1nn1ty.add@gmail.com>
2025-06-13 12:59:25 +0300

    пока без api

--- api/channels/router.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter, Depends, HTTPException
-from telethon import TelegramClient
-from client import get_telegram_client
-from starlette import status
-
-
-router = APIRouter(prefix="/users")
